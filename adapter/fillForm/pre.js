05dbd1d9064eb7dbcf33b83064303dbeefc5ee55
Soham Roy <jubi.developer.01@gmail.com>
2020-04-27 19:26:06 +0530

    change

--- adapter/fillForm/pre.js
+++ adapter/fillForm/pre.js
@@ -15,17 +15,17 @@ module.exports = {
     },
     lastMsg: (modal)=>{
         return new Promise(async function (resolve, reject){
+            // modal.reply= {
+            //     type: "text",
+            //     text:modal.tags.name+", Your Input Email is "+modal.tags.email
+            // },
+            // modal.reply= {
+            //     type: "text",
+            //     text:"Your input phone Number is "+modal.tags.email
+            // },  
             modal.reply= {
                 type: "text",
-                text:modal.tags.name+", Your Input Email is "+modal.tags.email
-            },
-            modal.reply= {
-                type: "text",
-                text:"Your input phone Number is "+modal.tags.email
-            },  
-            modal.reply= {
-                type: "text",
-                text:"Your data is sent to our 52 linked companies.Now you can close the window"
+                text:modal.tags.name+", Your Input Email is "+modal.tags.email+" Your input phone Number is "+modal.tags.phone+" Your data is sent to our 52 linked companies.Now you can close the window"
             }                 
                 
                 resolve(modal)

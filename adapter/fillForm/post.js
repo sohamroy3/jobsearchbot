cbb8b97566290bcd3ace37b987b0aaeea39e7501
Soham Roy <jubi.developer.01@gmail.com>
2020-04-27 15:56:50 +0530

    change

--- adapter/fillForm/post.js
+++ adapter/fillForm/post.js
@@ -2,9 +2,17 @@ module.exports = {
     emailValidation: (modal)=>{
         return new Promise(async function (resolve, reject){
             let regex=/^(.+)@(.+)$/;
+            if(modal.data.match(regex)){
+                console.log("Matchedddddddd")
+                delete(modal.stage)
+                resolve(modal)  
+            }
+            else{
+                console.log("Wrong email...!")
+                reject(modal)
+            }
             console.log(modal);
-            delete(modal.stage)
-            resolve(modal)        
+                  
         })
     },
            

901a7616ab7e900ffa19c085dc57836afc4dbf72
Soham Roy <jubi.developer.01@gmail.com>
2020-04-27 16:39:45 +0530

    change

--- adapter/fillForm/pre.js
+++ adapter/fillForm/pre.js
@@ -3,7 +3,7 @@ module.exports = {
         return new Promise(async function (resolve, reject){
             modal.reply= {
                 type: "text",
-                text:"Enter your name"
+                text:"Enter your email "+modal.tags.name
             }                 
                 
                 resolve(modal)  

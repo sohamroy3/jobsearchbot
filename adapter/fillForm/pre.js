177ca536c9e4edfd423aaf5e22108e07a0e6422d
Soham Roy <jubi.developer.01@gmail.com>
2020-05-05 21:28:43 +0530

    change

--- adapter/fillForm/pre.js
+++ adapter/fillForm/pre.js
@@ -25,7 +25,7 @@ module.exports = {
             // },  
             modal.reply= {
                 type: "text",
-                text:modal.tags.name+", Your Input Email is "+modal.tags.email+" Your input phone Number is "+modal.tags.phone+" Your data is sent to our 52 linked companies.Now you can close the window"
+                text:modal.tags.name+", Your Input Email is "+modal.tags.email+" |break|Your input phone Number is "+modal.tags.phone+" |break|Your data is sent to our 52 linked companies.Now you can close the window"
             }                 
                 
                 resolve(modal)

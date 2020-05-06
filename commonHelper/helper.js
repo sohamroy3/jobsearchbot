efb978c7a104ebd68cc7acbaafe588e0b7656bd3
Soham Roy <jubi.developer.01@gmail.com>
2020-05-06 17:06:45 +0530

    change

--- commonHelper/helper.js
+++ commonHelper/helper.js
@@ -1,32 +1,25 @@
-module.exports.pincodeInfo = function pInfo(pincode){
-    return new Promise(function(resolve, reject){
-        var request = require("request");
-  
-        var options = { 
-            method: 'GET',
-            url: 'http://postalpincode.in/api/pincode/' + pincode,
-            headers: 
-                {
+module.exports={
+   
+   
+    pincodeInfo:(pincode)=>{
+        return new Promise(function(resolve, reject){
+            var request = require("request");
+    
+            var options = { 
+                method: 'GET',
+                url: 'http://postalpincode.in/api/pincode/' + pincode,
+                headers: 
+                    {
 
-                } 
-        };
-  
-        request(options, function (error, response, body) {
-          if (error) throw new Error(error);
-          console.log(body);
-          try{//{"Message":"No records found","Status":"Error","PostOffice":null}
-            if(JSON.parse(body).Status == "Success"){
-              console.log("PIN API WORKING...")
-              resolve(JSON.parse(body).PostOffice[0])
-            }
-            else{
-             resolve("Error") 
-            }
-          }
-          catch(e){
-            resolve("Error")
-          }
-          });
-  
-    })
+                    } 
+            };
+    
+            request(options, function (error, response) {
+            if (error) throw new Error(error);
+            console.log(response.body);
+            return response.body.status;
+            });
+    
+        })
+    }
 }
